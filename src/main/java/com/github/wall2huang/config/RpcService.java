ae36f5086495866fdd577d93e244a6fae63d05eb
wall2huang <sb13590853671>
2017-07-17 22:58:48 +0800

    wall:init

---
+++ src/main/java/com/github/wall2huang/config/RpcService.java
@@ -0,0 +1,20 @@
+package com.github.wall2huang.config;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface RpcService
+{
+    String value();
+}
---
+++ src/main/java/com/github/wall2huang/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.github.wall2huang.service;/**
+ * Created by Administrator on 2017/7/17.
+ */
+
+/**
+ * author : Administrator
+ **/
+public class HelloService
+{
+
+
+}
